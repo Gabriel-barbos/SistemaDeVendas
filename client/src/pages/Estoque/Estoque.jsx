4005e08a756edd3f01404bd16551a9bb778dd20a
gabriel-barbos <gabriel.barbosa79@outlook.com>
2025-06-03 10:54:05 -0300

    feat: new stock ui

--- client/src/pages/Estoque/Estoque.jsx
+++ client/src/pages/Estoque/Estoque.jsx
@@ -1,50 +1,64 @@
 import React from 'react';
-import { ShoppingOutlined } from '@ant-design/icons';
+
+
+import { BarcodeOutlined, ShopOutlined, DollarCircleOutlined, AppstoreOutlined, LineChartOutlined } from '@ant-design/icons';
 import ProductCardStock from '../../components/ProductCardStock';
 import './Estoque.css';
 import useProducts from '../Produtos/useProducts';
-import EmptyData from  '../../assets/EmptyData.gif'
+import EmptyData from '../../assets/EmptyData.gif';
 
 function Estoque() {
   const { products, loading, error, updateProduct } = useProducts();
 
   return (
     <div>
-      <div className="Page-title">
-        <ShoppingOutlined style={{ fontSize: 25, marginRight: 5 }} />
-        <h1>Estoque</h1>
-      </div>
+      <h1>Estoque</h1>
+      
+      <div className="cards-group">
+        <div className="stock-card items">
+          <div className="card-icon">
+            <BarcodeOutlined />
+          </div>
+          <div className="card-content">
+            <h3 className="card-title">Total de Itens</h3>
+            <p className="card-value">109</p>
+            <span className="card-subtitle">produtos cadastrados</span>
+          </div>
+        </div>
 
-      <div className="stock-list">
-        {products.length > 0 ? (
-          products.map((produto) => (
-            <ProductCardStock 
-              key={produto._id} 
-              produto={produto} 
-              updateProduct={updateProduct} 
-            />
-          ))
-        ) : (
-          !loading && (
-            <div
-              className="empty-purchase-list"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "10px",
-              }}
-            >
-              <img
-                src={EmptyData}
-                alt="Nenhum item"
-                style={{ height: "100%", maxHeight: "150px", marginBottom: "5px" }}
-              />
-              <p style={{ fontSize: 20, fontWeight: 600 }}>Nenhum produto encontrado no estoque</p>
-            </div>
-          )
-        )}
+        <div className="stock-card value">
+          <div className="card-icon">
+            <DollarCircleOutlined />
+          </div>
+          <div className="card-content">
+            <h3 className="card-title">Valor do estoque</h3>
+            <p className="card-value">R$ 347.071,35</p>
+            <span className="card-subtitle">Valor total em estoque</span>
+          </div>
+        </div>
+
+        <div className="stock-card ">
+          <div className="card-icon">
+            <ShopOutlined />
+          </div>
+          <div className="card-content">
+            <h3 className="card-title"> Valor do estoque (venda)</h3>
+            <p className="card-value">R$ 387.071,35</p>
+            <span className="card-subtitle">valor de venda do estoque</span>
+          </div>
+        </div>
+
+        <div className="stock-card ">
+          <div className="card-icon">
+          <LineChartOutlined />
+          
+                    </div>
+          <div className="card-content">
+            <h3 className="card-title">Margem de lucro</h3>
+            <p className="card-value">15%</p>
+            <span className="card-subtitle">margem de lucro estoque x valor de venda</span>
+          </div>
+        </div>
       </div>
     </div>
   );
