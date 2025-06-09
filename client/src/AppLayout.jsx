979bc65b060d09f9d35e990a4c1a46ae866f5be9
gabriel-barbos <gabriel.barbosa79@outlook.com>
2025-06-09 10:39:53 -0300

    deploy

--- client/src/AppLayout.jsx
+++ client/src/AppLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from "../src/assets/logo.jpeg"
+import logo from "../src/assets/logo.png"
 import './main.css';
 import {
   PieChartOutlined,
